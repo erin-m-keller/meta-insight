a4af3d0cc8edfc3954cd8b9cd7b6b377803488ee
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-10 21:12:30 -0400

    add: adding file, router requirement, and exports

---
+++ controllers/api/gamereviewRoutes.js
@@ -0,0 +1,16 @@
+// import express router
+const gamereviewRouter = require("express").Router();
+
+// gamereviewRouter.get("/", async (req, res) => {
+//   // create route
+//   try {
+//     res.render("create", {
+//       // render create.handlebars
+//     });
+//   } catch (err) {
+//     // catch errors
+//     res.status(500).json(err); // return error
+//   }
+// });
+
+module.exports = gamereviewRouter; // export gamereview router
