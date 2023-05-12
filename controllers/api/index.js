b4aad66fb9e7b635b261a389968c3c83db16717e
Erin Keller <aestheticartist@gmail.com>
2023-05-11 20:46:16 -0400

    Added routes, auth and views for logging in and logging out

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,7 @@
-const router = require('express').Router();
+const router = require('express').Router(),      
+      authRoutes = require('./authRoutes');
+
+router.use('/auth', authRoutes);
 // const userRoutes = require('./userRoutes');
 // const postRoutes = require('./postRoutes');
 // const commentRoutes = require('./commentRoutes');
--- controllers/indexRoutes.js
+++ controllers/indexRoutes.js
@@ -1,11 +1,26 @@
-const router = require("express").Router(); // import express router
+const router = require("express").Router(),
+      withAuth = require('../utils/auth');
 
 router.get("/", async (req, res) => {
+  // login route
+  try {
+    if (req.session.logged_in) {
+      res.redirect('/homepage'); // render homepage.handlebars
+      return;
+    }
+    res.render('login'); // render login.handlebars
+  } catch (err) {
+    // catch errors
+    res.status(500).json(err); // return error
+  }
+});
+
+router.get("/homepage", withAuth, async (req, res) => {
   // homepage route
   try {
-    res.render("homepage", {
-      // render homepage.handlebars
-    });
+    res.render('homepage', {
+      logged_in: req.session.logged_in,
+    }); // render homepage.handlebars
   } catch (err) {
     // catch errors
     res.status(500).json(err); // return error
