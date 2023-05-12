62fe9bdc4d9e68ee8aaabc2138c130710e3f8165
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-11 20:39:31 -0400

    add: creating seed data for the database table gamereview

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,15 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User,Gamereviews } = require('../models');
 
 const userData = require('./userData.json');
+const gamereviewData = require('./gamereviewData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  await Gamereviews.bulkCreate(gamereviewData, {
+    returning: true,
+  });
 
   await User.bulkCreate(userData, {
     individualHooks: true,
